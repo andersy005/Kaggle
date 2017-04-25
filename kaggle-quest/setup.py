c3650b3cd44e2bd376dd1b3187f72341c8730d9c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-04-25 15:20:46 -0500

    add setup file

---
+++ kaggle-quest/setup.py
@@ -0,0 +1,7 @@
+# This file will be used to define some system specific parameters used in
+# the module
+
+# Set the location of the directory in which the module is located. For example in my case it is:
+# module_location = '/Users/username/anaconda/lib/python2.7/site-packages/kaggle-quest'
+
+# Update for your case:
